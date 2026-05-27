adb53686c481b9cff007cff41116f4bdd4d67001
Manuel Martínez <mmartinez@comparaonline.com>
2026-05-27 16:58:51 -0500

    chore: set app identity (SerenApp, com.serenapp.app)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,15 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'serenapp',
-  webDir: 'dist'
+  appId: 'com.serenapp.app',
+  appName: 'SerenApp',
+  webDir: 'dist',
+  plugins: {
+    LocalNotifications: {
+      smallIcon: 'ic_stat_serenapp',
+      iconColor: '#7B68A6',
+    },
+  },
 };
 
 export default config;
